50c484c72027c0971d598f73d591482e7c97dbb0
abishek-encipherhealth <abishek@encipherhealth.com>
2026-09-13 15:04:35 +0530

    Dummy commit for building

--- src/main/java/com/dev/orchestration_service/test/TestController.java
+++ src/main/java/com/dev/orchestration_service/test/TestController.java
@@ -27,7 +27,7 @@ public class TestController
     public String ping()
     {
         log.info("Received ping request");
-        String response = "pong from v5.0";
+        String response = "pong";
         log.info("Returning response: {}", response);
         return response;
     }
